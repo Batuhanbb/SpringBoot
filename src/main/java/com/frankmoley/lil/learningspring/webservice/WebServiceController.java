c74f64b780c3020fcad9b73ee1719d28f61ec044
BATUKE KE <batuhnbayir@gmail.com>
2022-05-03 13:16:29 +0300

    Initial commit

---
+++ src/main/java/com/frankmoley/lil/learningspring/webservice/WebServiceController.java
@@ -0,0 +1,29 @@
+package com.frankmoley.lil.learningspring.webservice;
+
+import com.frankmoley.lil.learningspring.business.ReservationService;
+import com.frankmoley.lil.learningspring.business.RoomReservation;
+import com.frankmoley.lil.learningspring.util.DateUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebServiceController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public WebServiceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(String dateString){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        return this.reservationService.getRoomReservationsForDate(date);
+    }
+}
